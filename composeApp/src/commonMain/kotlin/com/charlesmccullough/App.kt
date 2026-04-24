30566d28f16b6edac07d7abbc6f90becaea11033
Charles McCullough <charles.mccullough@gmail.com>
2026-04-24 12:01:20 -0500

    Installing Koin

--- composeApp/src/commonMain/kotlin/com/charlesmccullough/App.kt
+++ composeApp/src/commonMain/kotlin/com/charlesmccullough/App.kt
@@ -1,42 +1,18 @@
 package com.charlesmccullough
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.charlesmccullough.bookpedia.book.data.network.KtorRemoteBookDataSource
-import com.charlesmccullough.bookpedia.book.data.repository.DefaultBookRepository
 import com.charlesmccullough.bookpedia.book.presentation.book_list.bookpedia.book.presentation.book_list.BookListScreenRoot
 import com.charlesmccullough.bookpedia.book.presentation.book_list.bookpedia.book.presentation.book_list.BookListViewModel
-import com.charlesmccullough.bookpedia.core.data.HttpClientFactory
-import io.ktor.client.engine.HttpClientEngine
-import org.jetbrains.compose.resources.painterResource
-
-import kmp_book_tutorial.composeapp.generated.resources.Res
-import kmp_book_tutorial.composeapp.generated.resources.compose_multiplatform
+import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 @Preview
-fun App(engine: HttpClientEngine) {
+fun App() {
+    val viewModel = koinViewModel<BookListViewModel>()
+
     BookListScreenRoot(
-        viewModel = remember { BookListViewModel(
-            bookRepository = DefaultBookRepository(
-                remoteBookDataSource = KtorRemoteBookDataSource(
-                    httpClient = HttpClientFactory.create(engine)
-                )
-            )
-        ) },
+        viewModel = viewModel,
         onBookClick = {
 
         }
---
+++ composeApp/src/commonMain/kotlin/com/charlesmccullough/bookpedia/di/Modules.kt
@@ -0,0 +1,24 @@
+package com.charlesmccullough.bookpedia.di
+
+
+
+import com.charlesmccullough.bookpedia.book.data.network.KtorRemoteBookDataSource
+import com.charlesmccullough.bookpedia.book.data.network.RemoteBookDataSource
+import com.charlesmccullough.bookpedia.book.data.repository.DefaultBookRepository
+import com.charlesmccullough.bookpedia.book.domain.BookRepository
+import com.charlesmccullough.bookpedia.book.presentation.book_list.bookpedia.book.presentation.book_list.BookListViewModel
+import com.charlesmccullough.bookpedia.core.data.HttpClientFactory
+import org.koin.core.module.Module
+import org.koin.core.module.dsl.singleOf
+import org.koin.core.module.dsl.viewModelOf
+import org.koin.dsl.bind
+import org.koin.dsl.module
+
+expect val platformModule: Module
+
+val sharedModule = module {
+    single { HttpClientFactory.create(get()) }
+    singleOf(::KtorRemoteBookDataSource).bind<RemoteBookDataSource>()
+    singleOf(::DefaultBookRepository).bind<BookRepository>()
+    viewModelOf(::BookListViewModel)
+}
